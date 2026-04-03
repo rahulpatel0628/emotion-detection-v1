9d69ce7bd1043ba0443a084767adf2bd1d854428
Patel Rahul <rahulpatel16092005@gmail.com>
2026-04-03 17:49:48 +0530

    add dagshub token

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -23,15 +23,20 @@ logger = logging.getLogger(__name__)
 # -------------------- MLflow Setup --------------------
 def setup_mlflow():
     try:
-        dagshub.init(
-            repo_owner='rahulpatel16092005',
-            repo_name='mlops-mini-project',
-            mlflow=True
-        )
+        dagshub_token = os.getenv("DAGSHUB_PAT")
+        if not dagshub_token:
+            raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
 
-        mlflow.set_tracking_uri(
-            "https://dagshub.com/rahulpatel16092005/mlops-mini-project.mlflow"
-        )
+        os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+        os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+        dagshub_url = "https://dagshub.com"
+        repo_owner = "rahulpatel16092005"
+        repo_name = "mlops-mini-project"
+
+        # Set up MLflow tracking URI
+        mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+
 
         mlflow.set_experiment("DVC Pipeline Classification")
         logger.info("MLflow setup completed")

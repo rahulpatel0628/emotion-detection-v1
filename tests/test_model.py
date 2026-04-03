c86148edacf36c05abe93257377d8b8f6a2aa185
Patel Rahul <rahulpatel16092005@gmail.com>
2026-04-03 18:45:51 +0530

    add minor change

--- tests/test_model.py
+++ tests/test_model.py
@@ -29,7 +29,7 @@ class TestModelLoading(unittest.TestCase):
 
         cls.vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
 
-        cls.holdout_data = pd.read_csv('data/processed/test_bow.csv')
+        cls.holdout_data = pd.read_csv('data/features/test_bow.csv')
 
     @staticmethod
     def get_latest_model_version(model_name, stage="Staging"):
